d6003ee2455d17ae1122ffbc1ea9ca1f4d4eb831
Vishal Singh <octaculus.tech@gmail.com>
2023-12-26 23:01:47 +0530

    1st commit to the new branch b2'

---
+++ src/main/java/com/catchifo/main/controller/MainController.java
@@ -0,0 +1,12 @@
+package com.catchifo.main.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class MainController {
+    @GetMapping("/")
+    public String index(){
+        return "index";
+    }
+}
